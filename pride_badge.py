cc576cc8a3129986166ad659a8902d775478d938
Andreas 'count' Kotes <berlincount@users.noreply.github.com>
2024-12-27 18:07:40 +0100

    use modularized code for pride badge

--- pride_badge.py
+++ pride_badge.py
@@ -1,31 +1,29 @@
-# A name badge with customisable Pride flag background.
+## we have a set of default includes, including color names etc
+from includes import *
 
-from picographics import PicoGraphics, DISPLAY_TUFTY_2040
-import math
-
+### Overridables
 POWERMANAGEMENT = True
 
-display = PicoGraphics(display=DISPLAY_TUFTY_2040)
+BACKGROUND_COLOR = BLACK
 
-WIDTH, HEIGHT = display.get_bounds()
+name_config = {
+  "color": WHITE,
+  "max_size": 20,
+  "max_width": WIDTH-20,
+  "x_pos": WIDTH/2,
+  "y_pos": 10,
+}
 
-# List of available pen colours, add more if necessary
-RED = display.create_pen(209, 34, 41)
-ORANGE = display.create_pen(246, 138, 30)
-YELLOW = display.create_pen(255, 216, 0)
-GREEN = display.create_pen(0, 121, 64)
-INDIGO = display.create_pen(36, 64, 142)
-VIOLET = display.create_pen(115, 41, 130)
-WHITE = display.create_pen(255, 255, 255)
-PINK = display.create_pen(255, 175, 200)
-BLUE = display.create_pen(116, 215, 238)
-BROWN = display.create_pen(97, 57, 21)
-BLACK = display.create_pen(0, 0, 0)
-MAGENTA = display.create_pen(255, 33, 140)
-CYAN = display.create_pen(33, 177, 255)
-AMETHYST = display.create_pen(156, 89, 209)
+pronouns_config = {
+  "color": WHITE,
+  "max_size": 20,
+  "max_width": WIDTH-60,
+  "x_pos": WIDTH/2,
+  "y_pos": 175,
+}
 
-# Uncomment one of these to change flag
+# A name badge with customisable Pride flag background.
+
 # If adding your own, colour order is left to right (or top to bottom)
 COLOUR_ORDER = [RED, ORANGE, YELLOW, GREEN, INDIGO, VIOLET]  # traditional pride flag
 # COLOUR_ORDER = [BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, INDIGO, VIOLET]  # Philadelphia pride flag
@@ -43,28 +41,18 @@ FIRST_CHEVRON_HEIGHT = 0.4
 # Change this for vertical stripes
 STRIPES_DIRECTION = "horizontal"
 
-# Details are autoloaded
-try:
-    file = open("badge.txt", "r")
-except OSError:
-    with open("badge.txt", "w") as f:
-        f.write("edit badge.txt :)\n")
-        f.write("pronouns can be given\n")
-        f.write("and social as well\n")
-        f.flush()
-    file = open("badge.txt", "r")
+###
 
-# Read the name in any case
-NAME = file.readline().strip()
-# Try reading the pronouns
-try:
-  PRONOUNS = file.readline().strip()
-except:
-  PRONOUNS = ""
+# load badge entries from file
+name_config["text"], pronouns_config["text"], _ = load_badge()
 
-# Change the colour of the text (swapping these works better on a light background)
-TEXT_COLOUR = WHITE
-DROP_SHADOW_COLOUR = BLACK
+# calculate sizes of texts
+name_config["size"],name_config["width"]         = measure_text(display,name_config)
+pronouns_config["size"],pronouns_config["width"] = measure_text(display,pronouns_config)
+
+# position texts relative to each other
+name_config["x_pos"] = (WIDTH-name_config["width"])/2+10
+pronouns_config["x_pos"] = (WIDTH-pronouns_config["width"])/2
 
 # Draw the flag
 if STRIPES_DIRECTION == "horizontal":
@@ -80,6 +68,7 @@ if STRIPES_DIRECTION == "vertical":
         display.rectangle(stripe_width * x, 0, stripe_width, HEIGHT)
 
 if len(CHEVRONS) > 0:
+    import math
     stripe_width = round((HEIGHT * (1 - FIRST_CHEVRON_HEIGHT)) / len(CHEVRONS))
     offset = -stripe_width * math.floor((len(CHEVRONS) + 1) / 2)
     middle = round(HEIGHT / 2)
@@ -90,102 +79,10 @@ if len(CHEVRONS) > 0:
             (x + 1) * stripe_width + offset + middle, middle,
             x * stripe_width + offset, HEIGHT + stripe_width)
 
-# Set a starting scale for text size.
-# This is intentionally bigger than will fit on the screen, we'll shrink it to fit.
-name_size = 20
-pronouns_size = 20
-
-# These loops adjust the scale of the text until it fits on the screen
-while True:
-    display.set_font("bitmap8")
-    name_length = display.measure_text(NAME, name_size)
-    if name_length >= WIDTH - 20:
-        name_size -= 1
-    else:
-        # comment out this section if you hate drop shadow
-        DROP_SHADOW_OFFSET = 5
-        display.set_pen(DROP_SHADOW_COLOUR)
-        display.text(NAME, int((WIDTH - name_length) / 2 + 10) - DROP_SHADOW_OFFSET, 10 + DROP_SHADOW_OFFSET, WIDTH, name_size)
-
-        # draw name and stop looping
-        display.set_pen(TEXT_COLOUR)
-        display.text(NAME, int((WIDTH - name_length) / 2 + 10), 10, WIDTH, name_size)
-        break
-
-while True:
-    display.set_font("bitmap8")
-    pronouns_length = display.measure_text(PRONOUNS, pronouns_size)
-    if pronouns_length >= WIDTH - 60:
-        pronouns_size -= 1
-    else:
-        # draw pronouns and stop looping
-        display.set_pen(TEXT_COLOUR)
-        display.text(PRONOUNS, int((WIDTH - pronouns_length) / 2), 175, WIDTH, pronouns_size)
-        break
-
 # Once all the adjusting and drawing is done, update the display.
+draw_text(display,name_config)
+draw_text(display,pronouns_config)
 display.update()
 
-###
-# automatic backlight management
-import time
-import micropython
-from machine import ADC, Pin
-
-display.set_backlight(1.0)
-BACKLIGHT_LOW = micropython.const(0.375)
-BACKLIGHT_HIGH = micropython.const(1.0)
-LUMINANCE_LOW = micropython.const(384)
-LUMINANCE_HIGH = micropython.const(2048)
-LOW_BATTERY_VOLTAGE = micropython.const(3.1)
-lux_vref_pwr = Pin(27, Pin.OUT)
-lux = ADC(Pin(26))
-vbat_adc = ADC(Pin(29))
-vref_adc = ADC(Pin(28))
-usb_power = Pin(24, Pin.IN)
-
-# Returns a tuple of the raw luminance value, and the brightness to now use.
-def auto_brightness(previous: float) -> (float, float):
-    luminance = lux.read_u16()
-    luminance_frac = max(0.0, float(luminance - LUMINANCE_LOW))
-    luminance_frac = min(1.0, luminance_frac / (LUMINANCE_HIGH - LUMINANCE_LOW))
-    backlight = BACKLIGHT_LOW + (luminance_frac * (BACKLIGHT_HIGH - BACKLIGHT_LOW))
-    # Use the previous value to smooth out changes to reduce flickering.
-    # The "32" value here controls how quickly it reacts (larger = slower).
-    # The rate at which the main loop calls us also affects that!
-    backlight_diff = backlight - previous
-    backlight = previous + (backlight_diff * (1.0 / 32.0))
-    return (luminance, backlight)
-
-
-# Returns a tuple of voltage (fake value if on USB), "is on USB", and "is low".
-def measure_battery() -> (float, bool, bool):
-    if usb_power.value():
-        return (5.0, True, False)
-
-    # See the battery.py example for how this works.
-    vdd = 1.24 * (65535 / vref_adc.read_u16())
-    vbat = ((vbat_adc.read_u16() / 65535) * 3 * vdd)
-
-    low_battery = False
-    if vbat < LOW_BATTERY_VOLTAGE:
-        low_battery = True
-    return (vbat, False, low_battery)
-
-backlight = BACKLIGHT_LOW
-print("entering backlight loop")
-while POWERMANAGEMENT:
-    # Turn on VREF and LUX only while we measure things.
-    lux_vref_pwr.value(1)
-    (vbat, on_usb, low_battery) = measure_battery()
-    if low_battery:
-        backlight = BACKLIGHT_LOW
-    else:
-        (luminance, backlight) = auto_brightness(backlight)
-    lux_vref_pwr.value(0)
-
-    # Set the new backlight value.
-    display.set_backlight(backlight)
-
-    # measure every 100ms
-    time.sleep(0.1)
+# run an event loop with glimmer, powermanagement, etc
+main_loop(POWERMANAGEMENT,LED_GLIMMER)
